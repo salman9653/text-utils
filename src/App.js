58e51f5d2f5f96686440dc966ce6be0b35c6dab3
Salman <salman9915189734@gmail.com>
2022-11-09 21:47:11 +0530

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from './components/Navbar';
+// import TextForm from './components/TextForm';
+// import Markdown from './components/Markdown';
+import About from './components/About';
+// import Alert from './components/Alert';
 
 function App() {
+  const [mode , setMode] = useState("light");
+  const[alert, setAlert] = useState(null);
+  function showAlert(message,type){
+    setAlert({
+      message : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+  function toggleMode(){
+    if(mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#243743";
+      showAlert("Dark mode Applied" , "success");
+     }else {
+       setMode("light");
+       document.body.style.backgroundColor = "white";
+       showAlert("Light mode Applied" , "success");
+     }
+  }
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+        <Navbar title="TeXt-Utils" mode={mode} toggleMode={toggleMode}/>
+        {/* <Alert alert={alert} /> */}
+        <div className="container my-5">
+          <Routes>
+            {/* <Route path="/TextUtils" element={<TextForm heading="Enter Your Text" alert={showAlert} mode={mode} />} /> */}
+            {/* <Route path="/markdown" element={<Markdown heading="Markdown Editor" alert={showAlert} mode={mode} />} /> */}
+            <Route path="/about" element={<About alert={showAlert} mode={mode} />} />
+          </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
