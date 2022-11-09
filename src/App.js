a1c15f4046353d10fac8ec1a76b8c71973dfe99b
Salman <salman9915189734@gmail.com>
2022-11-09 21:53:00 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ import About from './components/About';
 function App() {
   const [mode , setMode] = useState("light");
   // const[alert, setAlert] = useState(null);
-  function showAlert(message,type){
-    setAlert({
-      message : message,
-      type : type
-    })
-    setTimeout(() => {
-      setAlert(null);
-    }, 2000);
-  }
+  // function showAlert(message,type){
+  //   setAlert({
+  //     message : message,
+  //     type : type
+  //   })
+  //   setTimeout(() => {
+  //     setAlert(null);
+  //   }, 2000);
+  // }
   function toggleMode(){
     if(mode === "light") {
       setMode("dark");
